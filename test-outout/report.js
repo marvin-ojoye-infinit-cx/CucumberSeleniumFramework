598822d54167fc6688eacde4980ca030d2c4438d
marvin-ojoye-infinit-cx <90262411+marvin-ojoye-infinit-cx@users.noreply.github.com>
2021-09-23 20:26:04 +0200

    move Samples in a diffrent Folder

--- test-outout/report.js
+++ test-outout/report.js
@@ -1,22 +1,10 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("/Users/NaveenKhunteta/Documents/workspace/FreeCrmBDDFramework/src/main/java/Features/taggedhooks.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/main/java/CocumberSamples.Features/taggedhooks.feature");
 formatter.feature({
   "line": 1,
   "name": "Test Tagged Hooks",
   "description": "",
   "id": "test-tagged-hooks",
   "keyword": "Feature"
-});
-formatter.before({
-  "duration": 259317,
-  "status": "passed"
-});
-formatter.before({
-  "duration": 133065,
-  "status": "passed"
-});
-formatter.before({
-  "duration": 135775,
-  "status": "passed"
 });
 formatter.scenario({
   "line": 4,
@@ -47,50 +35,17 @@ formatter.step({
   "name": "this is the third step",
   "keyword": "Then "
 });
-formatter.match({
-  "location": "TaggedHooksStepDefinition.this_is_the_first_step()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 85898436,
-  "status": "passed"
+  "status": "undefined"
 });
-formatter.match({
-  "location": "TaggedHooksStepDefinition.this_is_the_second_step()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 98039,
-  "status": "passed"
+  "status": "undefined"
 });
-formatter.match({
-  "location": "TaggedHooksStepDefinition.this_is_the_third_step()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 84479,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 54473,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 44919,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 81175,
-  "status": "passed"
-});
-formatter.before({
-  "duration": 112120,
-  "status": "passed"
-});
-formatter.before({
-  "duration": 82098,
-  "status": "passed"
-});
-formatter.before({
-  "duration": 76810,
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.scenario({
   "line": 10,
@@ -121,50 +76,17 @@ formatter.step({
   "name": "this is the third step",
   "keyword": "Then "
 });
-formatter.match({
-  "location": "TaggedHooksStepDefinition.this_is_the_first_step()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 127029,
-  "status": "passed"
+  "status": "undefined"
 });
-formatter.match({
-  "location": "TaggedHooksStepDefinition.this_is_the_second_step()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 88211,
-  "status": "passed"
+  "status": "undefined"
 });
-formatter.match({
-  "location": "TaggedHooksStepDefinition.this_is_the_third_step()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 88937,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 92284,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 140453,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 66290,
-  "status": "passed"
-});
-formatter.before({
-  "duration": 120620,
-  "status": "passed"
-});
-formatter.before({
-  "duration": 76143,
-  "status": "passed"
-});
-formatter.before({
-  "duration": 67536,
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.scenario({
   "line": 16,
@@ -195,37 +117,16 @@ formatter.step({
   "name": "this is the third step",
   "keyword": "Then "
 });
-formatter.match({
-  "location": "TaggedHooksStepDefinition.this_is_the_first_step()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 69572,
-  "status": "passed"
+  "status": "undefined"
 });
-formatter.match({
-  "location": "TaggedHooksStepDefinition.this_is_the_second_step()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 72834,
-  "status": "passed"
+  "status": "undefined"
 });
-formatter.match({
-  "location": "TaggedHooksStepDefinition.this_is_the_third_step()"
-});
+formatter.match({});
 formatter.result({
-  "duration": 71562,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 74866,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 67355,
-  "status": "passed"
-});
-formatter.after({
-  "duration": 66706,
-  "status": "passed"
+  "status": "undefined"
 });
 });
