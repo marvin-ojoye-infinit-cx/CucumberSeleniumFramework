8c45905d877c0b38089511f0de29cdf076d0c879
marvin-ojoye-infinit-cx <90262411+marvin-ojoye-infinit-cx@users.noreply.github.com>
2021-09-24 15:41:13 +0200

    new test description

---
+++ src/main/java/cx/infinit/selenium/stepDefinitions/MynewTest.java
@@ -0,0 +1,53 @@
+//package cx.infinit.selenium.stepDefinitions;
+//
+//import cucumber.api.java.en.Given;
+//import cucumber.api.java.en.When;
+//import cucumber.api.java.en.Then;
+//import cucumber.api.DataTable;
+//import cucumber.api.PendingException;
+//import cucumber.api.java.en.And;
+//import cucumber.api.java.en.But;
+//
+//public class MynewTest {
+//	@Then("-user enters username and password$")
+//	public void user_enters_username_and_password(DataTable arg1) throws Throwable {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    // For automatic transformation, change DataTable to one of
+//	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+//	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+//	}
+//
+//	@Then("^-user clicks on login button$")
+//	public void user_clicks_on_login_button() throws Throwable {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+//	}
+//
+//	@Then("^user is on home page$")
+//	public void user_is_on_home_page() throws Throwable {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+//	}
+//
+//	@Then("^user moves to new deal page$")
+//	public void user_moves_to_new_deal_page() throws Throwable {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+//	}
+//
+//	@Then("^user enters deal details$")
+//	public void user_enters_deal_details(DataTable arg1) throws Throwable {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    // For automatic transformation, change DataTable to one of
+//	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+//	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+//	    throw new PendingException();
+//	}
+//
+//	@Then("^Close the browser$")
+//	public void close_the_browser() throws Throwable {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new PendingException();
+//	}
+//
+//}
